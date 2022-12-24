012d8454c49263952adbb8f719e60052f4dc93f5
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-24 09:19:35 -0300

    feat: operadores aritméticos p2

---
+++ aula09.py
@@ -0,0 +1,11 @@
+concatenacao = 'Yago' + ' Fogaça'
+print(concatenacao)
+
+# O sinal de multiplicação ( * ) serve como repitição para o tipo str
+
+a_dez_vezes = 'A' * 10
+trez_vezes_concatenacao = (concatenacao + '  ') * 3
+
+print(a_dez_vezes)
+print("----------------")
+print(trez_vezes_concatenacao)
