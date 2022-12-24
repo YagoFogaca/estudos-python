a1bf431af6cd03461b547c60a0948d6282178b44
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-24 09:09:26 -0300

    feat: operadores aritméticos p1

---
+++ aula08.py
@@ -0,0 +1,28 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 3  # float, sempre irá retornar um numero de ponto flutuante
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 3  # int, sempre irá retornar um numero inteiro, mas caso o numero da divisão tenha ponto flutuante, o retornar também sera flutuante, mas sem as casas decimais como da divisão normal, linha 10
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # resto da divisão
+print('Módulo', modulo)
+
+
+# Como saber se um numero é divisível por outro? Multiplo
+
+print(10 % 5 == 0)
+print(10 % 2 == 0)
+print(300 % 3 == 0)
+print(16 % 8 == 0)
