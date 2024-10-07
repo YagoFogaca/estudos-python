01ade3af6d408053f1a5cecb77834fb311c08621
Yago Fogaça <yagofogaca20@gmail.com>
2024-10-07 19:51:46 -0300

    feat: dia 01

---
+++ aula1.py
@@ -0,0 +1,29 @@
+# Comentarios
+""" 
+Docstrings
+
+"""
+
+print('Hello world!')
+
+# Strings = texto que estão envolvidos entre aspas. str -> string -> texto;
+
+#Int = números inteiros. int -> números
+#Float = números decimais, números com virgula, números não inteiros. float -> número;
+
+# **Os números negativos se encaixam tanto int como float;
+
+# Bool = valor positivo ou negativo. bool -> booleano -> valor (True ou false);
+
+
+# Conversão de tipos de dados:
+int('21') # str -> int
+float(1) # int -> float
+str(21) # int -> str
+bool('true') # Qualquer dado passador será true, valores vazios ou 0 será negativo;
+
+
+# Variavel são usadas para salvar algo na memoria do computador.
+
+teste = bool(4)
+print(teste)
